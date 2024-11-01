694b2148872781b7dca97a7db07ec671aa173cae
Mohammad Arab <mohammad.arab.dev@gmail.com>
2024-11-01 14:58:01 +0330

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./Containers/Header";
 import productListing from "./Containers/productListing";
